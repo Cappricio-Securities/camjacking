95c622211d7d4facaf45c5e62aab9f0299025f04
karthikeyan.V <33289300+karthi-the-hacker@users.noreply.github.com>
2023-04-23 00:54:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /**
  * CamJacking
  * CamJacking is a tool designed for use in human penetration testing tool. It is intended to simulate potential security threats by testing the negligence of people, and is used to identify weaknesses in an organization's security infrastructure. By using CamJacking, security professionals can gain a better understanding of their organization's potential risks and take proactive measures to improve their security posture.
